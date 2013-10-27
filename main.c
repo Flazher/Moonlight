f2295e0bb7ca19cb45e61095cd537a998aedec1e
Flazher <flazhik@gmail.com>
2013-10-27 17:39:51 +0700

    Updated display init function, implemented background drawing

--- main.c
+++ main.c
@@ -3,48 +3,94 @@
 #include <errno.h>
 #include <string.h>
 #include <X11/Xlib.h>
+#include <X11/cursorfont.h>
 
-struct MoonlightPropsStruct
+typedef struct {
+	GC gc;
+	XGCValues GCvalues;
+	int x, y, Width, Height;
+	Drawable drawable;
+	unsigned long DefaultGCMask;
+} DrawingContext;
+
+typedef struct 
 {
 	Display *Display;
 	int DefaultScreen;
 	Window Root;
-	XWindowAttributes RootAttributes;
-	Pixmap BackgroundPixmap;
-};
+	DrawingContext DC;
+	Window BackgroundWindow;
+	unsigned long DefaultEventMask;
+} MoonlightProps;
 
-typedef struct MoonlightPropsStruct MoonlightProps;
-typedef struct MonitorStruct Monitor;
-
-static void MoonlightDisplayInit (MoonlightProps *Moonlight);
+void MoonlightDisplayInit (MoonlightProps *Moonlight);
+static void MoonlightDisplayTerminate (MoonlightProps *Moonlight);
 
 MoonlightProps Moonlight;
 
-static void MoonlightDisplayInit (MoonlightProps *Moonlight)
+void
+MoonlightDisplayInit (MoonlightProps *Moonlight)
 {
-	if (!(Moonlight->Display = XOpenDisplay(NULL)))
+	MoonlightProps *M = Moonlight;
+	if (!(M->Display = XOpenDisplay(NULL)))
 	{
 		printf ("Connection to X failed. Is $DISPLAY variable (%s) correct?",
 			getenv("DISPLAY"));
 		exit (1);
 	}
+	
+	M->DefaultScreen = DefaultScreen (M->Display);
+	M->Root = RootWindow (M->Display, M->DefaultScreen);
+	M->DefaultEventMask = ExposureMask | KeyPressMask;
 
-	Moonlight->DefaultScreen = DefaultScreen(Moonlight->Display);
-	Moonlight->Root = RootWindow(Moonlight->Display, Moonlight->DefaultScreen);
-	if (!XGetWindowAttributes(Moonlight->Display, Moonlight->Root, &Moonlight->RootAttributes))
+	M->DC.GCvalues.function   = GXxor; 
+	M->DC.GCvalues.background = WhitePixel (M->Display, M->DefaultScreen);
+	M->DC.GCvalues.foreground = WhitePixel (M->Display, M->DefaultScreen);
+	M->DC.GCvalues.plane_mask = AllPlanes;
+	M->DC.Width = DisplayWidth (M->Display, M->DefaultScreen);
+	M->DC.Height = DisplayHeight (M->Display, M->DefaultScreen);
+	
+	M->DC.DefaultGCMask = 
+		GCFunction | GCPlaneMask | GCBackground | GCForeground;
+	
+	XEvent ev;
+	
+	M->BackgroundWindow = XCreateSimpleWindow
+	(
+		M->Display, M->Root, 0, 0, M->DC.Width, M->DC.Height,
+		0, M->DC.GCvalues.background, M->DC.GCvalues.background
+	);
+
+	XMapWindow(M->Display, M->BackgroundWindow);
+
+	M->DC.gc = XCreateGC
+		(M->Display, M->BackgroundWindow, M->DC.DefaultGCMask, &M->DC.GCvalues);	
+	
+	XFlush (M->Display);
+	XSelectInput(M->Display, M->BackgroundWindow, M->DefaultEventMask);
+	
+	while (1)
 	{
-		printf("Can't get default screen attributes. Quiting.\n");
-		exit (1);
+		XNextEvent(M->Display, &ev);
+		
+		if (ev.type == Expose) {
+			XFillRectangle(M->Display, M->BackgroundWindow, M->DC.gc, 0, 0, 500, 500);
+		}
+		if (ev.type == KeyPress)
+			break;
 	}
-
-	//Moonlight->BackgroundPixmap = XCreatePixmap(Moonlight->Display, Moonlight->Root, Moonlight->RootAttributes.width, Moonlight->RootAttributes.height, 0);
 }
 
+static void
+MoonlightDisplayTerminate (MoonlightProps *Moonlight)
+{
+	XFreeGC (Moonlight->Display, Moonlight->DC.gc);
+}
 
 int main(int argc, char **argv)
 {
 	MoonlightDisplayInit (&Moonlight);
-	
+	MoonlightDisplayTerminate (&Moonlight);
 	XCloseDisplay (Moonlight.Display);
 
 	return 0;
